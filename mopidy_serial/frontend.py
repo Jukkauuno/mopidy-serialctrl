82015680926dd796eb143c8204a1f23bd86c8424
Jaakko Karjalainen <jaakko.e.karjalainen@gmail.com>
2015-02-03 14:00:07 +0000

    Logging added

--- mopidy_serial/frontend.py
+++ mopidy_serial/frontend.py
@@ -1,16 +1,23 @@
 import pykka
 import serial
+import logging
 
 from mopidy import core
 
-class SerialFrontend(pykka.ThreadingActor, core.coreListener):
+logger = logging.getLogger(__name__)
+
+class SerialFrontend(pykka.ThreadingActor, core.CoreListener):
 	def __init__(self, config, core):
 		super(SerialFrontend, self).__init__()
 		self.core = core
-	
+		
+		logger.info('Setting serial...')
+		
 		# Serial setup
 		self.ser = serial.Serial((config['serial']['port']), (config['serial']['baud']))
 		
+		logger.info('Serial set!')
+
 		self.current_volume = 0
 
 		# Channel initialization
@@ -18,6 +25,8 @@ class SerialFrontend(pykka.ThreadingActor, core.coreListener):
 		self.channel_current = 0
 		self.channel_1 = None
 		self.channel_2 = None
+
+		self.main_loop()
 
 	def get_channels(self):
 		self.playlists = []
@@ -34,22 +43,30 @@ class SerialFrontend(pykka.ThreadingActor, core.coreListener):
 		self.core.playback.play()	
 
 	def main_loop(self):
-		self.message = self.ser.readline()
+		
+		logger.info('Entered main loop')
 
-		self.volume, self.channel = self.message.split(',')
+		while(1>0):
+			self.message = self.ser.readline()
 
-		# Set volume
-		if self.volume != current_volume:
-			self.core.playback.volume = self.volume
-			self.current_volume = self.volume
-		else:
-			self.current_volume = self.current_volume
+			self.volume, self.channel = self.message.split(',')
+
+			logger.info('Volume:', self.volume)
+
+			# Set volume
+			if self.volume != current_volume:
+				self.core.playback.volume = self.volume
+				self.current_volume = self.volume
+			else:
+				self.current_volume = self.current_volume
 
 			
-		if self.channel != self.current_channel:
-			if self.channel == '1':
-				self.set_channels(channel_1)
-			elif self.channel == '2':
-				self.set_channels(channel_2)
-			else:
-				self.ser.printline("CHN SET ERR")
+			if self.channel != self.current_channel:
+				if self.channel == '1':
+					self.set_channels(channel_1)
+				elif self.channel == '2':
+					self.set_channels(channel_2)
+				else:
+					self.ser.printline("CHN SET ERR")
+
+		self.ser.close()
